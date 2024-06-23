f2567912b529a3b6b83c42b038c819459bb1976e
qool_loop <bob.nem>
2024-06-23 10:41:01 +0300

    Merging branches

--- src/components/burgerMenu/BurgerMenu.jsx
+++ src/components/burgerMenu/BurgerMenu.jsx
@@ -1,6 +1,5 @@
 import { scaleDown as Menu } from 'react-burger-menu';
 import {NavLink} from "react-router-dom";
-import {BsInfoSquare} from 'react-icons/bs';
 import {BiMap} from 'react-icons/bi';
 import Logo from "../images/noun-hexahedron-1159998.svg";
 import './burgerMenu.css';
@@ -11,7 +10,6 @@ const BurgerMenu = (props) => {
           <div className='bm-logo'>
             <NavLink to="/"><img src={Logo} alt='logotype'/></NavLink>
           </div>
-          <NavLink to="/about" activeClassName="bm-item-active"><BsInfoSquare size=".8em" className="bm-icon" /> About</NavLink>
           <div style={{
             fontSize: '1.2rem',
             marginLeft: '2rem',
